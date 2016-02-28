4e21d7c4621b64c3caf1ec3f0ae3304f644b9c63
arao <araobp@users.noreply.github.com>
2016-02-27 17:08:05 -0800

    runtest.sh

---
+++ runtest.sh
@@ -0,0 +1,6 @@
+DIR='./tests/tega'
+python $DIR/test_tree.py
+python $DIR/test_util.py
+python $DIR/test_idb.py
+python $DIR/test_driver.py
+
--- tests/tega/runtest.sh
+++
@@ -1,5 +0,0 @@
-python test_tree.py
-python test_util.py
-python test_idb.py
-python test_driver.py
-
